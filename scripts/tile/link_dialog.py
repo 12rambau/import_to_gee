c1f3d0d82e981b40924a116e2ad95331aa261111
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2020-12-14 16:00:19 +0000

    improve link behaviour

--- scripts/tile/link_dialog.py
+++ scripts/tile/link_dialog.py
@@ -39,21 +39,21 @@ class LinkDialog(sw.SepalWidget, v.Dialog):
         self.aoi_tile.aoi_select_btn.observe(self.fire_dialog, 'loading')
     
     # the pyperclip method does not work with SEPAL
-    def select_all(self, widget, data, event):
-        
-        # copy the data to clipboard
-        pyperclip.copy(self.link)
-        
-        # select all the link to prevent pyperclip failures
-        
-        return
+    #def select_all(self, widget, data, event):
+    #    
+    #    # copy the data to clipboard
+    #    pyperclip.copy(self.link)
+    #    
+    #    # select all the link to prevent pyperclip failures
+    #    
+    #    return
     
     def fire_dialog(self, link):
         
         # the toggle btn has changed let's see if it's for a good reason
         if self.aoi_tile.output.type == 'success':
         
-            self.value = True,
+            self.value = True
             self.link.v_model = self.aoi_tile.io.assetId.replace('projects/earthengine-legacy/assets/', '')
             
         return
